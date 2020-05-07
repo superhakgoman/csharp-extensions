5c11cc36ba002486026e07b53006e78822af9a51
superhakgoman <superhakgoman@gmail.com>
2020-05-07 17:22:39 +0900

    Update Extension.cs

--- Extension.cs
+++ Extension.cs
@@ -5,6 +5,7 @@ namespace CsharpExtensions
         public static bool IsValid(this string str) => string.IsNullOrEmpty(str) == false;
         public static bool IsValid<T>(this T[] array) => array != null && array.Length > 0;
         public static bool IsEmpty<T>(this T[] array) => array != null && array.Length == 0;
+        public static bool IsValid<T>(this IEnumerable<T> enumerable) => enumerable != null && enumerable.Count() > 0;
 
     }
 }
